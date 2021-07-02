f08cf4762ba67c36c607b38d1917dafd84def5d0
andikscript <andikscript@gmail.com>
2021-07-02 18:55:39 +0700

    commit Operator

--- Operator/main.go
+++ Operator/main.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	// operator aritmatika
+	// untuk operasi angka
+	var a, b = 12, 15
+
+	fmt.Println(a + b)
+	fmt.Println(a - b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
+
+	// operator pembanding
+	// menghasilkan nilai true atau false
+
+	fmt.Println(a == b)
+	fmt.Println(a <= b)
+	fmt.Println(a >= b)
+	fmt.Println(a != b)
+	fmt.Println(a < b)
+	fmt.Println(a > b)
+
+	// operator logika
+	// untuk operasi variable yang bernilai true atau false
+	var c, d = true, true
+
+	fmt.Println(c && d)
+	fmt.Println(c || d)
+	fmt.Println(!c)
 }
